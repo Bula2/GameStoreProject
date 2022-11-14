a4034885d3738013e6fcaa02a639552ed4f49808
Bula2 <bulaev123da@gmail.com>
2022-11-14 22:02:04 +0300

    shop page upd

--- front/src/components/ShopPage/Game/Game.tsx
+++ front/src/components/ShopPage/Game/Game.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./game.module.scss";
-import {Image} from "antd";
+import {Button, Image} from "antd";
 import Link from "next/link";
+import cx from "classnames";
 
 interface IGame {
   id: number,
@@ -12,17 +13,31 @@ interface IGame {
 }
 
 const Game: React.FC<IGame> = (props) => {
+
+  const [alert, useAlert] = useState(true)
+
+  const showAlert = () => {
+    useAlert(false)
+    setTimeout(() => useAlert(true), 2000)
+  }
+
   return (
     <div className={styles.item} key={props.id}>
-      <Image
-        className={styles.item_img}
-        width={200}
-        height={250}
-        src={props.src}
-      />
-      <div className={styles.item_title}>{props.title}</div>
-      <div className={styles.item_text}>{props.platform}</div>
-      <div className={styles.img_text}>{props.price}</div>
+      <div className={styles.item_img}>
+        <Image
+          className={styles.item_img}
+          width={200}
+          height={250}
+          src={props.src}
+        />
+      </div>
+      <div className={styles.item_info}>
+        <div className={styles.item_title}>{props.title}</div>
+        <div className={styles.item_platform}>{props.platform}</div>
+        <div className={styles.item_price}>{props.price} ₽</div>
+        <div className={styles.item_button} onClick={() => showAlert()}>Добавить в корзину</div>
+      </div>
+      <div className={cx(alert && styles.hide, styles.alert)}>Игра добавлена в корзину</div>
     </div>
   );
 };
